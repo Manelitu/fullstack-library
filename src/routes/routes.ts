02da8305ec2305b654ee52763f34f7bc574c2d5c
Manelitu <emanuelwork1@gmail.com>
2022-08-24 20:47:37 -0300

    Install and config Cors. [feat]

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,11 @@
 import { Application } from 'express';
 
+const cors = require('cors');
 const booksRoute = require('./booksRoutes');
 const express = require('express');
 const app: Application = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/books', booksRoute);
